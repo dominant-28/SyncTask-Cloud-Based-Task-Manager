a7fb4957a4e2b3149775ae3885720735e3212fe5
dominant-28 <sohamkale2828@gmail.com>
2025-06-02 00:17:28 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ const PORT=process.env.PORT|| 6000
 const app=express()
 app.use(
     cors({
-    origin:['http://localhost:3000','http://localhost:3001'],
+    origin:['http://localhost:3000','http://localhost:3001','https://the-task-manager.onrender.com'],
     methods:['GET',"POST","PUT","DELETE"],
     credentials:true,
 }))
